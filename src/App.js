0d09d466d3f80ef20b2bc6b134b1c7399993961b
PJMantoss <joelptoss@gmail.com>
2022-08-15 09:24:28 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
   return (
     <main>
       <div className='dieContainer'>
-        <Die>1</Die>
-        <Die>2</Die>
-        <Die>3</Die>
-        <Die>4</Die>
-        <Die>5</Die>
-        <Die>6</Die>
-        <Die>7</Die>
-        <Die>8</Die>
-        <Die>9</Die>
-        <Die>10</Die>
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={7} />
+        <Die value={8} />
+        <Die value={9} />
+        <Die value={10} />
       </div>
     </main>
   );
