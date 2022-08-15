0f8ded626c1f60017465829b5e23621144e5fe39
PJMantoss <joelptoss@gmail.com>
2022-08-15 08:58:39 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import Die from './components/Die';
 import './App.css';
 
 function App() {
   return (
     <main>
-      <div className='dieContainer'></div>
+      <div className='dieContainer'>
+        <Die>1</Die>
+        <Die>2</Die>
+        <Die>3</Die>
+        <Die>4</Die>
+        <Die>5</Die>
+        <Die>6</Die>
+      </div>
     </main>
   );
 }
