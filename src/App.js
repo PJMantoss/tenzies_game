d01382c1dd53e174aa6841fd84417872250c768f
PJMantoss <joelptoss@gmail.com>
2022-08-16 13:02:57 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -22,15 +22,6 @@ function App() {
         {diceNumbers.map(diceNum => {
           return <Die value={diceNum} />
         })}
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={7} />
-        <Die value={8} />
-        <Die value={9} />
-        <Die value={10} />
       </div>
     </main>
   );
