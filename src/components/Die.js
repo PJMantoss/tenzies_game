08aa8163683a50e6d3ebcb469eed6054c057351a
PJMantoss <joelptoss@gmail.com>
2022-08-18 20:47:23 +0100

    update Die

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Die = ({value, isHeld}) => {
+const Die = ({value, isHeld, holdDice}) => {
 
   const styles = {
     backgroundColor: isHeld === true ? '#59e391' : 'white'
