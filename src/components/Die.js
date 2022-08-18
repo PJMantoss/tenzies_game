94d0c24c133dd7f70ea260deb54f140f53768e59
PJMantoss <joelptoss@gmail.com>
2022-08-18 20:32:24 +0100

    update Die

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Die = ({value, isHeld}) => {
+
+  const style = {
+    backgroundColor: '#59e391'
+  }
   return (
     <div className={isHeld === true ? 'is-held' : 'die'}>
         <h2>{value}</h2>
