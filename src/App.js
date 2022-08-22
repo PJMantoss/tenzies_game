997fe8df4ffaba2c4d660bde422577e18543e433
PJMantoss <joelptoss@gmail.com>
2022-08-22 11:42:54 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App() {
       <p className='instructions'>Roll until all dice are the same. Click each die to freeze it
       at its current value between rolls.</p>
 
-      <div className='dieContainer'>
+      <div className='diceContainer'>
         {diceNumbers.map(diceNum => {
           return <Die 
                     value={diceNum.value} 
