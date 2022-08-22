1150cc20998623bb62c337a1b25d68a3830d3d47
PJMantoss <joelptoss@gmail.com>
2022-08-22 11:30:36 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
   return (
     <main>
       <h1 className='title'>Tenzies</h1>
-      <p className='instructions'>Roll until all dice aree the same. Click each die to freeze it
+      <p className='instructions'>Roll until all dice are the same. Click each die to freeze it
       at its current value between rolls.</p>
 
       <div className='dieContainer'>
